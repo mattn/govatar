b3feb1bacfdf935ead7df5f5ec1493ab574586bc
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2016-12-13 09:10:06 +0900

    fix signature for ActionFunc

--- govatar/main.go
+++ govatar/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"github.com/urfave/cli"
-	"github.com/o1egl/govatar"
+	"errors"
 	"os"
-	"log"
+
+	"github.com/o1egl/govatar"
+	"github.com/urfave/cli"
 )
 
 var version = "0.1.0-dev"
@@ -18,18 +18,18 @@ func main() {
 	app.Author = "Oleg Lobanov"
 	app.Commands = []cli.Command{
 		{
-			Name:    "generate",
+			Name:      "generate",
 			ArgsUsage: "<(male|m)|(female|f)>",
-			Aliases: []string{"g"},
-			Usage:   "Generates random avatar",
+			Aliases:   []string{"g"},
+			Usage:     "Generates random avatar",
 			Flags: []cli.Flag{
 				cli.StringFlag{
-					Name: "output,o",
+					Name:  "output,o",
 					Value: "avatar.png",
 					Usage: "Output file name",
 				},
 			},
-			Action: func(c *cli.Context) {
+			Action: func(c *cli.Context) error {
 				var g govatar.Gender
 				switch c.Args().First() {
 				case "male", "m":
@@ -37,13 +37,13 @@ func main() {
 				case "female", "f":
 					g = govatar.FEMALE
 				default:
-					fmt.Println("Incorrect gender param. Run `govatar help generate`")
-					os.Exit(0)
+					return errors.New("Incorrect gender param. Run `govatar help generate`")
 				}
 				err := govatar.GenerateFile(g, c.String("output"))
 				if err != nil {
-					log.Fatal(err)
+					return err
 				}
+				return nil
 			},
 		},
 	}
